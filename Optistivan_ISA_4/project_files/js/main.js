92b18b2be68e8d307391d8c3deedd2db82eb10b2
Michael Arledge <arledgemike@gmail.com>
2014-06-26 17:12:14 -0500

    sync files

--- Optistivan_ISA_4/project_files/js/main.js
+++ Optistivan_ISA_4/project_files/js/main.js
@@ -1,23 +1,44 @@
 var isa = {
   
+  
   init: function(){
   
     console.log("isa called");
-  
   },
   
   configure_click: function(event){
   
   },
-
+  show_controls_efficacy: function(show){
+    if(show){
+      $('#link_controls').fadeIn();
+    }
+  },
   lock_scroll: function(){
-    // lock for pages that do not need scrolling
     $(document).on('touchmove', false);
   },
 
   unlock_scroll: function(){
-    // start fullpage scolling
-    $('#container').fullpage();
-  }
+    $('#container').fullpage({
+      scrollingSpeed:1000,
+      onLeave: function(index, nextIndex, direction){
+	isa.animate_test_out(index, nextIndex, direction);
 
+      },
+      afterLoad: function(anchorLink, index){
+	isa.animate_test_in(anchorLink, index);
+      }
+      
+    });
+  },
+  animate_test_in: function(anchorLink, index){
+    $('.section.active').animate({
+      opacity:1
+    }); 
+  },
+  animate_test_out: function(index, nextIndex, direction){
+      $('.section ').eq(index -1).animate({
+	opacity:0      
+      },1000);
+    }
 }
