cd8162d14d2fbcd6e00b90523f90b0afcee17fda
Michael Arledge <arledgemike@gmail.com>
2014-07-02 17:36:17 -0500

    video page started

--- Optistivan_ISA_11/project_files/js/main.js
+++ Optistivan_ISA_11/project_files/js/main.js
@@ -1,4 +1,7 @@
 var isa = {
+  video_play: false,
+  touch_start: 0,
+  touch_end:0, 
   
   init: function(){
   
@@ -18,6 +21,43 @@ var isa = {
   unlock_scroll: function(){
     // start fullpage scolling
     $('#container').fullpage();
-  }
+  },
+  
+  setup_video: function(){
+    var v = document.getElementsByTagName("video")[0];
+    
+      $('.graph ul li.btn').on('click', function(){
+      rel = $(this).attr('rel');
 
+      console.log(rel);
+      $('#vid_container video').attr('src', rel);
+      $('#vid_container').fadeIn(1000, function(){
+        v.play();
+	isa.video_play = true;
+      });
+    });
+    $('.exit_btn, #vid_container').on('click', function(){
+      if(isa.video_play){
+        v.pause();
+        isa.video_play = false;
+      }else{
+        v.play();
+        isa.video_play = true;
+      }
+      console.log("clicked");
+    });
+  v.addEventListener("ended", function(){
+   v.pause();
+   isa.video_play = false;
+   $('#vid_container').addClass('end');
+   $('.end').on('click', function(){
+      $(this).fadeOut(function(){ $('#vid_container').removeClass('end');     });
+   });   
+  });
+
+
+  
+  
+  }
+  
 }
