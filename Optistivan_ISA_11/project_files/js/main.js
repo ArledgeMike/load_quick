61a81e264b37062428e43b27ae69a465c1727046
Michael Arledge <arledgemike@gmail.com>
2014-08-07 16:44:39 -0500

    add new charts

--- Optistivan_ISA_11/project_files/js/main.js
+++ Optistivan_ISA_11/project_files/js/main.js
@@ -43,29 +43,28 @@ var isa = {
 	   
 	  rel = $(this).attr('rel');
 	  bg = $(this).attr('data-bg');
-          
+          gif = $(this).attr('data-gif');
+	  id = $(this).attr('data-id');
+
 	  $('.graph ul li.btn.play').removeClass('play');
 	  $(this).addClass('play');
           console.log(rel,  bg);
           $('#vid_container video').attr('src', rel);
+	  $('.cell').fadeOut(function(){
+	     $('.cell').attr("id", id).css("background-image", "url(project_files/images/" + gif + ")").fadeIn();	
+	  
+	  });
           $('#background').fadeOut(function(){
+         	  
 	    $(this).css("background-image" , "url(project_files/images/"+ bg + ")" ).fadeIn();
+	     
 	  });
 	}
       });
      
 
-   hammer_object.on('click', function(){
-	   console.log("pause click");
-      if(isa.video_play){
-        isa.v.pause();
-        isa.video_play = false;
-      }else{
-        isa.v.play();
-        isa.video_play = true;
-      console.log("clicked");
-      }
-     });
+   hammer_object.on('click', isa.video_control);
+   
    $('#vid_container .exit_btn').on('click',function(){
      isa.v.pause();
      isa.v.currentTime = 0;
@@ -75,18 +74,30 @@ var isa = {
    isa.v.addEventListener("ended", isa.video_ended);
       
        
-    },
+  },
   change_background: function(){
   
+  },
+  video_control: function(){
+    if(isa.video_play){
+        isa.v.pause();
+        isa.video_play = false;
+      }else{
+        isa.v.play();
+        isa.video_play = true;
+      console.log("clicked");
+      }
+
   },
   video_ended: function(){
     isa.v.pause();
      isa.video_play = false;
+       hammer_object.off('click', isa.video_control);
   //   $('#vid_container').addClass('end');
   //   $('.end').on('click', function(){
   //     $(this).fadeOut(function(){ $('#vid_container').removeClass('end');     });
   //   });
-     hammer_object.on("swipeup", isa.clean_up_video);
+     //hammer_object.on("swipeup", isa.clean_up_video);
 
   },
   clean_up_video: function(event){
