d588533ec6f718d17c0cb2bab9842561a8ffaf49
ArledgeMike <arledgemike@gmail.com>
2014-08-19 09:34:00 -0500

    last night's commit

--- Optistivan_ISA_11/project_files/js/main.js
+++ Optistivan_ISA_11/project_files/js/main.js
@@ -4,109 +4,72 @@ var isa = {
   touch_end:0,
   v: "", 
   
-  init: function(){
-  
-    console.log("isa called");
-  
+  init: function(){  
+	
   },
-  
-  configure_click: function(event){
-  
-  },
-
   lock_scroll: function(){
-    // lock for pages that do not need scrolling
     $(document).on('touchmove', false);
   },
-
   unlock_scroll: function(){
-    // start fullpage scolling
     $('#container').fullpage();
   },
-  
   setup_video: function(){
-      isa.v = document.getElementsByTagName("video")[0];
-      
-      hammer_object = Hammer(isa.v);
+    isa.v = document.getElementsByTagName("video")[0];
+	hammer_object = Hammer(isa.v);
        
-      $('.graph ul li.btn').on('click', function(){
-        if ( $(this).hasClass('play') ){
-          
-	  console.log("play the movie");
-	  
-	  $('#vid_container').fadeIn(1000, function(){
-            isa.v.play();
-	    isa.video_play = true;
-          });
-
-	}else{
-	   
-	  rel = $(this).attr('rel');
-	  bg = $(this).attr('data-bg');
-          gif = $(this).attr('data-gif');
-	  id = $(this).attr('data-id');
-
-	  $('.graph ul li.btn.play').removeClass('play');
-	  $(this).addClass('play');
-          console.log(rel,  bg);
-          $('#vid_container video').attr('src', rel);
-	  $('.cell').fadeOut(function(){
-	     $('.cell').attr("id", id).css("background-image", "url(project_files/images/" + gif + ")").fadeIn();	
-	  
-	  });
-          $('#background').fadeOut(function(){
-         	  
-	    $(this).css("background-image" , "url(project_files/images/"+ bg + ")" ).fadeIn();
-	     
-	  });
-	}
-      });
+    $('.graph ul li.btn').on('click', function(){
+      if( $(this).hasClass('play') ){
+	    $('#vid_container').fadeIn(1000, function(){
+          isa.v.play();
+	      isa.video_play = true;
+        });
+	  }else{
+	    rel = $(this).attr('rel');
+	    bg = $(this).attr('data-bg');
+        gif = $(this).attr('data-gif');
+	    id = $(this).attr('data-id');
+		  
+		$('.graph ul li.btn.play').removeClass('play');
+	    $(this).addClass('play');
+        $('#vid_container video').attr('src', rel);
+	    $('.cell').fadeOut(function(){
+	      $('.cell').attr("id", id).css("background-image", "url(project_files/images/" + gif + ")").stop().fadeIn(1000);	
+	    });
+        $('#background').fadeOut(function(){   	  
+	      $(this).css("background-image" , "url(project_files/images/"+ bg + ")" ).fadeIn();
+	    });
+	  }
+    });
      
-
-   hammer_object.on('click', isa.video_control);
+    hammer_object.on('click', isa.video_control);
    
-   $('#vid_container .exit_btn').on('click',function(){
-     isa.v.pause();
-     isa.v.currentTime = 0;
-     isa.clean_up_video();
-   });  
+    $('#vid_container .exit_btn').on('click',function(){
+      isa.v.pause();
+      isa.v.currentTime = 0;
+      isa.clean_up_video();
+    });  
   
-   isa.v.addEventListener("ended", isa.video_ended);
-      
-       
-  },
-  change_background: function(){
-  
+    isa.v.addEventListener("ended", isa.video_ended);
+          
   },
   video_control: function(){
     if(isa.video_play){
-        isa.v.pause();
-        isa.video_play = false;
-      }else{
-        isa.v.play();
-        isa.video_play = true;
-      console.log("clicked");
-      }
-
+      isa.v.pause();
+      isa.video_play = false;
+    }else{
+      isa.v.play();
+      isa.video_play = true;
+    }
   },
   video_ended: function(){
     isa.v.pause();
-     isa.video_play = false;
-       hammer_object.off('click', isa.video_control);
-  //   $('#vid_container').addClass('end');
-  //   $('.end').on('click', function(){
-  //     $(this).fadeOut(function(){ $('#vid_container').removeClass('end');     });
-  //   });
-     //hammer_object.on("swipeup", isa.clean_up_video);
-
+    isa.video_play = false;
+    hammer_object.off('click', isa.video_control);
   },
   clean_up_video: function(event){
-  
-   $('#vid_container').fadeOut(function(){
+    $('#vid_container').fadeOut(function(){
 	  hammer_object.off("swipeup", isa.clean_up_video);
-	  console.log("cleaned up");
-
 	});
   }
  
-  }
+}
--- Optistivan_ISA_2/project_files/js/main.js
+++ Optistivan_ISA_2/project_files/js/main.js
@@ -1,101 +1,89 @@
 var isa = {
-	fpage_index: 1,
+  fpage_index: 1,
   
   init: function(){
-    console.log("isa called");
+	  
   },
-  
-  configure_click: function(event){
-  
-  },
+
   show_controls_efficacy: function(show){
     if(show){
       $('#link_controls').fadeIn();
     }
   },
+  
   lock_scroll: function(){
     $('#content').on('touchmove', false);
   },
 
-  unlock_scroll: function(){
-
- 
-   $('#container').fullpage({
-    
-   scrollingSpeed:1000,
+  unlock_scroll: function(){ 
+    $('#container').fullpage({
+      scrollingSpeed:1000,
       onLeave: function(index, nextIndex, direction){
-	      console.log("ON LEAVE" + index + nextIndex + direction);
-	      isa.fpage_index = nextIndex;
-	      console.log(isa.fpage_index);
-       if( (index >= 3) && (direction == "down") ){
-       $('#link_controls li a#mcrpc').removeClass('active');
-       $('#link_controls li a#nsclc').addClass('active');
-       }
-       if( (nextIndex == 3) && (direction == "up") ){
-         $('#link_controls li a#mcrpc').addClass('active');
-         $('#link_controls li a#nsclc').removeClass('active');
-       }	      
-
+	    isa.fpage_index = nextIndex;
+        if( (index >= 3) && (direction == "down") ){
+          $('#link_controls li a#mcrpc').removeClass('active');
+          $('#link_controls li a#nsclc').addClass('active');
+        }
+        if( (nextIndex == 3) && (direction == "up") ){
+          $('#link_controls li a#mcrpc').addClass('active');
+          $('#link_controls li a#nsclc').removeClass('active');
+        }	      
        isa.animate_test_out(index, nextIndex, direction);
-
       },
       afterLoad: function(anchorLink, index){
-	//      console.log("AFTER LOAD" + anchorLink + index);
-	isa.animate_test_in(anchorLink, index);
+	    isa.animate_test_in(anchorLink, index);
       }
-      
     });
-   $('#link_controls ul li a#mcrpc.icon').on("click", isa.go_to );
-   $('#link_controls ul li a#nsclc.icon').on("click", isa.go_to ); 
+    
+	$('#link_controls ul li a#mcrpc.icon').on("click", isa.go_to );
+    $('#link_controls ul li a#nsclc.icon').on("click", isa.go_to ); 
+    
+	if(location.hash == "#nsclc"){
+      $.fn.fullpage.moveTo(4, 0);
+      $('#link_controls li a#mcrpc').removeClass('active');
+      $('#link_controls li a#nsclc').addClass('active');
+    }
   },
   animate_test_in: function(anchorLink, index){
     $('.section.active').animate({
       opacity:1
     }); 
-
   },
   animate_test_out: function(index, nextIndex, direction){
-      $('.section ').eq(index -1).animate({
-	opacity:0      
-      },1000);
-    },
-  
+    $('.section ').eq(index -1).animate({
+	  opacity:0      
+    },1000);
+  },
   go_to: function(){
-	  console.log("go to called");
     switch(isa.fpage_index){
-   case 1:
-            $.fn.fullpage.moveTo(4, 0);
+      case 1:
+        $.fn.fullpage.moveTo(4, 0);
 	    break;	    
-   case 2:
-            $.fn.fullpage.moveTo(5);
-	   break;
-   case 3:
-            $.fn.fullpage.moveTo(6);
+      case 2:
+        $.fn.fullpage.moveTo(5);
+	    break;
+      case 3:
+        $.fn.fullpage.moveTo(6);
 	    break;	 
-   case 4:
-            $.fn.fullpage.moveTo(1);
+      case 4:
+        $.fn.fullpage.moveTo(1);
 	    break;	
-   case 5:
-            $.fn.fullpage.moveTo(2);
+      case 5:
+        $.fn.fullpage.moveTo(2);
 	    break;	
-   case 6:
-            $.fn.fullpage.moveTo(3);
+      case 6:
+        $.fn.fullpage.moveTo(3);
 	    break;	
     }
 
-
-   id = event.target.id;
-   if (id == "mcrpc"){
-	   $('#link_controls ul li a.active').removeClass('active');
-	   $('#link_controls ul li a#mcrpc').addClass('active');
-   }else{
-   	   $('#link_controls ul li a.active').removeClass('active');
-	   $('#link_controls ul li a#nsclc').addClass('active');
-
-   }
-   event.preventDefault(event);
-
+    id = event.target.id;
+    if (id == "mcrpc"){
+	  $('#link_controls ul li a.active').removeClass('active');
+	  $('#link_controls ul li a#mcrpc').addClass('active');
+    }else{
+   	  $('#link_controls ul li a.active').removeClass('active');
+	  $('#link_controls ul li a#nsclc').addClass('active');
+    }
+    event.preventDefault(event);
   }
-
-
 }
